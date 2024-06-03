b2aeaaed83c041fb1ef30ee1d9814719375aaadd
AnusreeVasudevan <anusreevasudevan02@gmail.com>
2024-06-03 21:31:26 +0530

    modified:   public/adminassets/js/custom-chart.js

--- public/adminassets/js/custom-chart.js
+++ public/adminassets/js/custom-chart.js
@@ -52,22 +52,7 @@
                             borderColor: 'rgba(44, 120, 220)',
                             data: [...yValues]
                         },
-                        {
-                            label: 'Visitors',
-                            tension: 0.3,
-                            fill: true,
-                            backgroundColor: 'rgba(4, 209, 130, 0.2)',
-                            borderColor: 'rgb(4, 209, 130)',
-                            data: [40, 20, 17, 9, 23, 35, 39, 30, 34, 25, 27, 17]
-                        },
-                        {
-                            label: 'Products',
-                            tension: 0.3,
-                            fill: true,
-                            backgroundColor: 'rgba(380, 200, 230, 0.2)',
-                            borderColor: 'rgb(380, 200, 230)',
-                            data: [30, 10, 27, 19, 33, 15, 19, 20, 24, 15, 37, 6]
-                        }
+                        
     
                     ]
                 },
@@ -102,22 +87,7 @@
                         borderColor: 'rgba(44, 120, 220)',
                         data: [...yValues]
                     },
-                    {
-                        label: 'Visitors',
-                        tension: 0.3,
-                        fill: true,
-                        backgroundColor: 'rgba(4, 209, 130, 0.2)',
-                        borderColor: 'rgb(4, 209, 130)',
-                        data: [40, 20, 17, 9, 23, 35, 39, 30, 34, 25, 27, 17]
-                    },
-                    {
-                        label: 'Products',
-                        tension: 0.3,
-                        fill: true,
-                        backgroundColor: 'rgba(380, 200, 230, 0.2)',
-                        borderColor: 'rgb(380, 200, 230)',
-                        data: [30, 10, 27, 19, 33, 15, 19, 20, 24, 15, 37, 6]
-                    }
+                    
 
                 ]
             },

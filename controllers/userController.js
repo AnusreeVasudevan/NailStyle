6a17ad1c58d9b7d87597d9c6a28aaa0a11d300de
AnusreeVasudevan <anusreevasudevan02@gmail.com>
2024-06-12 11:35:42 +0530

    modified:   controllers/userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -840,7 +840,6 @@ const downloadInvoice = async(req,res)=>{
         //console.log(844);
         const browser = await puppeteer.launch({
             headless: true,
-           executablePath:'/home/sagar/workplace/scraping-demo/node_modules/puppeteer/.local-chromium/linux-599821/chrome-linux/chrome',
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
           });
                   console.log(846);
